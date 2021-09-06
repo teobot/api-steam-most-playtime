7c0776366c716bc6facb420ff2d99a5c2c86889b
theo.clapperton <theo.clapperton@timpson.com>
2021-09-06 10:03:11 +0100

    Removed similar function, Using config for vars

--- src/api/steamdb.js
+++ src/api/steamdb.js
@@ -1,49 +1,54 @@
-const axios = require('axios');
+const axios = require("axios");
 
-// calculate the different in seconds between 2 dates
-const getTimeDifference = (date1, date2) => {
-    const diff = Math.abs(date2.getTime() - date1.getTime());
-    return Math.floor(diff / 1000);
-};
+// import shared functions
+const { getSecondsBetweenDates } = require("../functions/general.functions");
+
+const config = require("../config/config");
+
 
 const steamDB = axios.create({
-    baseURL: 'https://store.steampowered.com/',
+  baseURL: config.get("api.url"),
 });
 
 const getAppReviews = async (id, cursor) => {
-    try {
-        const req = await steamDB.request({
-            url: `/appreviews/${id}`, method: "get", params: {
-                json: "1",
-                filter: "recent",
-                cursor: encodeURI(cursor),
-                num_per_page: "100",
-                language: "all",
-                purchase_type: "all",
-                review_type: "all",
-                language: "english",
-            }
-        })
-        return req.data;
-    } catch (error) {
-        console.log("THROWING A ERROR HERE");
-        console.log(error);
-        return null;
-    }
-}
+  try {
+    const req = await steamDB.request({
+      url: `/appreviews/${id}`,
+      method: "get",
+      params: {
+        json: "1",
+        filter: "recent",
+        cursor: encodeURI(cursor),
+        num_per_page: "100",
+        language: "all",
+        purchase_type: "all",
+        review_type: "all",
+        language: "english",
+      },
+    });
+    return req.data;
+  } catch (error) {
+    console.log("THROWING A ERROR HERE");
+    console.log(error);
+    return null;
+  }
+};
 
-const getAllSteamGames = async () => {
-    let start = new Date();
+// this function returns a list for all the steam games, can be used to get the appid
+// const getAllSteamGames = async () => {
+//   let start = new Date();
 
-    try {
-        const req = await axios.get('https://api.steampowered.com/ISteamApps/GetAppList/v2');
-        return {games: req.data, time: getTimeDifference(start, new Date())};
-    } catch (error) {
-        console.log(error);
-    }
-}
+//   try {
+//     const req = await axios.get(
+//       "https://api.steampowered.com/ISteamApps/GetAppList/v2"
+//     );
+//     return { games: req.data, time: getSecondsBetweenDates(start, new Date()) };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 // export the getAppReviews function
 module.exports = {
-    getAppReviews,
-}
+  getAppReviews,
+};
