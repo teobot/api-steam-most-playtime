3b5ec5c12bd91fbac701c230d48bb39e2504ef31
theo.clapperton <theo.clapperton@timpson.com>
2021-09-06 15:18:45 +0100

    Added test route to get game info

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -3,9 +3,18 @@ const config = require("../config/config");
 const version = config.get("version");
 
 const controller = require("../controllers/review.controller");
+const steamApi = require("../api/steamdb");
 
 module.exports = function (app) {
-
   // This route creates a game and returns a joinable url and admin access token.
   app.route("/api/" + version + "/review").post(controller.getReview);
+
+  app.route("/api/" + version + "/game").get(async (req, res) => {
+    try {
+      const info = await steamApi.getGameInfo(req.body.game_id);
+      res.send(info.data);
+    } catch (error) {
+      res.send(error.message);
+    }
+  });
 };
