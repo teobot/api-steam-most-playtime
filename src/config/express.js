9f85ba040ea439782696cfe83df312c10af7df57
teobot <theoclapperton@outlook.com>
2021-10-01 12:08:19 +0100

    Updated headers

--- src/config/express.js
+++ src/config/express.js
@@ -7,7 +7,7 @@ const allowCrossOriginRequests = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, X-Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header(
     "Access-Control-Allow-Methods",
