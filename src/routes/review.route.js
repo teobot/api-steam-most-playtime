fe87807cd4a3b2fedda7923154ee919303ca7527
teobot <theoclapperton@outlook.com>
2021-10-01 12:06:39 +0100

    removed route

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -9,12 +9,12 @@ module.exports = function (app) {
   // This route creates a game and returns a joinable url and admin access token.
   app.route("/api/" + version + "/review").post(controller.getReview);
 
-  app.route("/api/" + version + "/game").get(async (req, res) => {
-    try {
-      const info = await steamApi.getGameInfo(req.body.game_id);
-      res.send(info.data);
-    } catch (error) {
-      res.send(error.message);
-    }
-  });
+  // app.route("/api/" + version + "/game").get(async (req, res) => {
+  //   try {
+  //     const info = await steamApi.getGameInfo(req.body.game_id);
+  //     res.send(info.data);
+  //   } catch (error) {
+  //     res.send(error.message);
+  //   }
+  // });
 };
